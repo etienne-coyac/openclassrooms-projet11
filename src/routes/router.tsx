eaca4ba52b86297c658c978298645d1b12c7cc41
Etienne <etienne.coyac@gmail.com>
2023-03-09 12:11:46 +0100

    feat: added outlet, not found route & images

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,23 +2,30 @@ import { createBrowserRouter } from "react-router-dom";
 import AboutPage from "../views/AboutPage";
 import HomePage from "../views/HomePage";
 import HousingPage from "../views/HousingPage";
+import Main from "../views/Main";
 import NotFoundPage from "../views/NotFoundPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />
-  },
-  {
-    path: "/not-found",
-    element: <NotFoundPage />
-  },
-  {
-    path: "/fiche-logement",
-    element: <HousingPage />
-  },
-  {
-    path: "/a-propos",
-    element: <AboutPage />
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "/fiche-logement",
+        element: <HousingPage />
+      },
+      {
+        path: "/a-propos",
+        element: <AboutPage />
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />
+      }
+    ]
   }
 ]);
---
+++ src/views/Main.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function Main() {
+  return (
+    <>
+      <h1>Bonsoir</h1>
+      <Outlet />
+    </>
+  );
+}
+
+export default Main;
